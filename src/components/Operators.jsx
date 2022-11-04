038a8b20ee2be948b8d4c46c0c764c68631e6222
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-04 14:25:22 +0000

    comments

--- src/components/Operators.jsx
+++ src/components/Operators.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ACTIONS } from "./Objects";
-
+// created an operator component(math signs) with props (the operator, class name, dispatch(which executes the function when the button matches an operator ))
+//  a payload which creates an object for adding the operator sign
 export default function Operators({ operator, span, dispatch }) {
   function addOperator() {
     dispatch({ type: ACTIONS.Operators, payload: { operator: operator } });
