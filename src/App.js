5380f0fe8a77362dcc0f30e10ae1a2466d2afac3
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-04 12:28:22 +0000

    import initial objects

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Buttons from './components/Buttons';
 import Operators from './components/Operators';
 import './components/Style.css';
 import reducer from'./components/reducer.js'
-const initial = {previousNumber: null, currentNumber: null, operators: null, startClicking: false}
-
 function App() {
   const[{previousNumber, currentNumber, operators}, dispatch]= useReducer(reducer, initial);
   const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
--- src/components/Objects.jsx
+++ src/components/Objects.jsx
@@ -1,15 +1,20 @@
-
 export const SIGNS = {
-    add: "+",
-    minus: "-",
-    mutiply: "*",
-    division: "÷",
-    pecentage: "%",
-  };
-  export const ACTIONS = {
-    AddNumbers: "Add-Numbers",
-    Operators: "operators",
-    Clear: "clear",
-    Calculate: "evaluate",
-    Delete: "delete"
-  }
+  add: "+",
+  minus: "-",
+  mutiply: "*",
+  division: "÷",
+  pecentage: "%",
+};
+export const ACTIONS = {
+  AddNumbers: "Add-Numbers",
+  Operators: "operators",
+  Clear: "clear",
+  Calculate: "evaluate",
+  Delete: "delete",
+};
+export const initial = {
+  previousNumber: null,
+  currentNumber: null,
+  operators: null,
+  startClicking: false,
+};
